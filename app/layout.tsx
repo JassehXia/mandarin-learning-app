7b5b05e02770bfef1f39cec16e13de97e67b6ce1
JassehXia <jassehxia@gmail.com>
2026-01-23 08:59:20 -0500

    page analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Playfair_Display, Inter } from "next/font/google";
 import { ClerkProvider } from '@clerk/nextjs'
+import { Analytics } from "@vercel/analytics/next"
 import "./globals.css";
 
 const playfair = Playfair_Display({ subsets: ["latin"], variable: "--font-playfair" });
